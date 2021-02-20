0ec3f53bc8e80b3560df49078db0f34261e3326a
makis.damigos@gmail.com <makis.damigos@gmail.com>
2021-02-20 15:57:19 +0200

    add some functionality to Subject class, check for more latter

--- Dependencies/Subject.m
+++ Dependencies/Subject.m
@@ -2,12 +2,14 @@ classdef Subject
     properties
         img {mustBeNumeric}
         reference {mustBeNumeric}
-        zscore {mustBeNumeric}
+        zscore {double}
         aff_hemisphere {mustBeNumeric}
         ss_hem_masks {logical}
         as_hem_masks {logical}
         aff_tranf 
         deformable_transform
+        outlier_detection {mustBeNumeric}
+        lesion_pred {mustBeNumeric}
     end
     
     methods
@@ -15,5 +17,10 @@ classdef Subject
         function obj = Subject(img)
             obj.img = img;
         end
+        
+%         function map_to_subject_obj(obj, subject_info)
+% %             obj.img = subject_info.subject;
+%             obj.zscore = subject_info.zscore;
+%         end
     end
 end
